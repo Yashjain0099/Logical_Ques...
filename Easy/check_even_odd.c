06b0341d1c824e7b9b381f1d1411c8de30960c3d
yashjain0099 <yashhjainofficial@gmail.com>
2025-09-16 16:57:02 +0530

    add check even odd program

---
+++ Easy/check_even_odd.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("enter a number");int n;
+	scanf("%d",&n);
+	if(n%2==0)printf("%d its a even number",n);
+	else printf("%d is not  a even number",n);
+	return 0;
+}
